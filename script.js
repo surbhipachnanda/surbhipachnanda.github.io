3b60eee2f585e675f646abbcc1eac9037c823aad
akanksha <akankshakaushal0899@gmail.com>
2019-09-01 17:40:28 +0530

    website pushed

---
+++ script.js
@@ -0,0 +1,17 @@
+var submitted= false;
+
+$(".menu-items").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  nextArrow: $(".next"),
+  prevArrow: $(".prev")
+});
+$(".menu-items1").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  nextArrow: $(".next1"),
+  prevArrow: $(".prev1")
+});
+
